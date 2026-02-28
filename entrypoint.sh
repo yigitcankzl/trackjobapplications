918b74e2acbb7b4ed33e4fbd7890e8bd435718ba
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-02-28 17:41:20 +0300

    remove redundant db check in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-echo "Waiting for database..."
-until python manage.py dbshell --command="SELECT 1;" > /dev/null 2>&1; do
-  echo "Database is unavailable - waiting..."
-  sleep 1
-done
-echo "Database is up!"
 
 echo "Making migrations..."
 python manage.py makemigrations --noinput

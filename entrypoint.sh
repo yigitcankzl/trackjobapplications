596f3f9c2af967366c465d115efc63918f3e6445
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-02-28 15:04:13 +0300

    Wait for DB and run makemigrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+echo "Waiting for database..."
+until python manage.py dbshell --command="SELECT 1;" > /dev/null 2>&1; do
+  echo "Database is unavailable - waiting..."
+  sleep 1
+done
+echo "Database is up!"
+
+echo "Making migrations..."
+python manage.py makemigrations --noinput
+
 echo "Running migrations..."
 python manage.py migrate --noinput
 

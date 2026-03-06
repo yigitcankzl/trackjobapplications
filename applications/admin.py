31f153f601b3ba0900882ad1cbe08a47e931266c
Yiğitcan Kızıl <kizilyigit33@gmail.com>
2026-03-06 22:12:53 +0300

    feat: add ApplicationNote inline and improve Application admin

--- applications/admin.py
+++ applications/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 
-from .models import Application
+from .models import Application, ApplicationNote
 
-admin.site.register(Application)
+
+class ApplicationNoteInline(admin.TabularInline):
+    model = ApplicationNote
+    extra = 0
+    readonly_fields = ("created_at",)
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ("company", "position", "status", "user", "applied_date")
+    list_filter = ("status", "source")
+    search_fields = ("company", "position")
+    inlines = [ApplicationNoteInline]

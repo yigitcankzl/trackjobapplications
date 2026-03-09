63de138350856b5ef0a8c595189e735dfc16760d
Yiğitcan Kızıl <kizilyigit33@gmail.com>
2026-03-09 22:19:29 +0300

    Add exception handling to NotificationPreference signal

--- trackjobapplications-backend/users/signals.py
+++ trackjobapplications-backend/users/signals.py
@@ -1,6 +1,10 @@
+import logging
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
+
+logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -8,4 +12,7 @@ def create_notification_preferences(sender, instance, created, **kwargs):
     if created:
         from users.models import NotificationPreference
 
-        NotificationPreference.objects.get_or_create(user=instance)
+        try:
+            NotificationPreference.objects.get_or_create(user=instance)
+        except Exception:
+            logger.exception("Failed to create NotificationPreference for user %s", instance.pk)

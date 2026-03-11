6b150cc1803b9b5ababd5a8e1838818c9bf26105
Yiğitcan Kızıl <kizilyigit33@gmail.com>
2026-03-11 21:19:23 +0300

    feat: switch email backend from SMTP to Resend API (HTTP-based)

--- trackjobapplications-backend/config/settings.py
+++ trackjobapplications-backend/config/settings.py
@@ -222,12 +222,8 @@ CRON_SECRET = os.environ.get("CRON_SECRET", "")
 
 # Email
 EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend")
-EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "TrackJobs <noreply@trackjobs.com>")
+RESEND_API_KEY = os.environ.get("RESEND_API_KEY", "")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "TrackJobs <onboarding@resend.dev>")
 
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.Argon2PasswordHasher",

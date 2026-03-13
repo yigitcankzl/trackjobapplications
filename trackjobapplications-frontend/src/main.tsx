e66360d213439635990956e3b708d06fca4ef16b
Yiğitcan Kızıl <kizilyigit33@gmail.com>
2026-03-13 17:34:56 +0300

    feat: add HelmetProvider and sync html lang with i18n

--- trackjobapplications-frontend/src/main.tsx
+++ trackjobapplications-frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import './lib/i18n'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>,
 )
 
